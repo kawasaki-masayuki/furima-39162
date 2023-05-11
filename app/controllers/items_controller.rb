b3eca39e056e51832374c4f3f009389c9eda6f33
kawasaki-masayuki <purple-wisteria@outlook.jp>
2023-05-11 16:53:19 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index]
 
   def index
-    #@items = Item.all ← 商品一覧表示機能で実装
+    @items = Item.all
   end
 
   def new
