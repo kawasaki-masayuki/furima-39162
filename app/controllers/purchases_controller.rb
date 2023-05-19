bae73bfd7236975f9b89fed0ad13e0f8438e49fd
kawasaki-masayuki <purple-wisteria@outlook.jp>
2023-05-19 18:19:16 +0900

    アクティブハッシュの修正実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -21,7 +21,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_delivery).permit(:post_code, :prefecture_id, :city, :address, :building_name, :telephone_number). merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_delivery).permit(:post_code, :prefecture_id, :city, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
